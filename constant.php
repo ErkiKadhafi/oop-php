a83524f142ac8136495897f812b7a082f3b0d880
ErkiKadhafi <erki.qaedhafi@gmail.com>
2021-07-16 07:16:33 +0700

    constant in php

---
+++ constant.php
@@ -0,0 +1,33 @@
+<?php
+    //define gabisa disimpen dalam kelas
+    // define("NAMA", "Michael Alexander");
+    // echo NAMA;
+
+    // echo "<br>";
+
+    // const UMUR = 32;
+    // echo UMUR;
+
+
+    //const bisa dipake dalem kelas
+    // class Coba{
+    //     const NAMA = "Michael Alexander";
+    // }
+
+    // echo Coba::NAMA;
+
+
+    //magic constant
+    // echo __LINE__; //menampilkan baris dimana line code ditulis
+    // echo __FILE__; //menampilkan fullpath file sekarang 
+    // function test(){
+    //     return __FUNCTION__; //menampilkan nama function yang dipanggil
+    // }
+    // class Test{
+    //     public $kelas = __CLASS__; //menampilkan nama kelas
+    // }
+    
+    // echo test();
+    // $obj = new Test();
+    // echo $obj->kelas;
+?>
